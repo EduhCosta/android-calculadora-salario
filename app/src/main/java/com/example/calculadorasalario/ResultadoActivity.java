7f9d7dfcb4279c0930c9a6530a5554c4187d3f81
Eduardo Costa <eduhcosta@live.com>
2020-10-10 10:33:22 -0300

    Finish result show data logic

--- app/src/main/java/com/example/calculadorasalario/ResultadoActivity.java
+++ app/src/main/java/com/example/calculadorasalario/ResultadoActivity.java
@@ -1,12 +1,20 @@
 package com.example.calculadorasalario;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.calculadorasalario.enums.IntentKeys;
+
+import org.w3c.dom.Text;
 
 public class ResultadoActivity extends AppCompatActivity {
     private double bruto;
@@ -23,6 +31,33 @@ public class ResultadoActivity extends AppCompatActivity {
         this.getIntentMessages();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.setMessagesOnView();
+    }
+
+
+    private void setMessagesOnView() {
+        this.setMessageOnElement(R.id.valSalarioBruto, this.bruto);
+        this.setMessageOnElement(R.id.valINSS,-1 * this.inss);
+        this.setMessageOnElement(R.id.valIRRF, -1 * this.irrf);
+        this.setMessageOnElement(R.id.valDescontos, -1 * this.descontos);
+        this.setMessageOnElement(R.id.valSalarioLiquido, this.liquido);
+        this.setMessageOnElement(R.id.valPercentDescontos, this.percentDecontos());
+    }
+
+    private double percentDecontos() {
+        return 100 - ((this.liquido / this.bruto) * 100);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setMessageOnElement(int id, double value) {
+        TextView input = findViewById(id);
+        input.setText(Double.toString(value));
+    }
+
+
     private void getIntentMessages() {
         Intent msgIntent = getIntent();
         this.bruto = getMessageFromIntent(IntentKeys.SALARIO_BRUTO, msgIntent);
@@ -33,9 +68,7 @@ public class ResultadoActivity extends AppCompatActivity {
     }
 
     private double getMessageFromIntent(IntentKeys key, Intent intent) {
-        String msg = intent.getStringExtra(key.getValor());
-        double doubleParsed = Double.parseDouble(msg);
-        return doubleParsed;
+        return intent.getDoubleExtra(key.getValor(), 0);
     }
 
     public void onClickVoltar(View view) {
